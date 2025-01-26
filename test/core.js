7dd460272ee07c9c98314302f0d03a1c9ff477fa
Mastaaa <125365308+Mastaaa1987@users.noreply.github.com>
2025-01-27 00:44:27 +0100

    test

---
+++ test/core.js
@@ -0,0 +1,12 @@
+var dglow=document.getElementById("dglow");
+
+function myFunction(){
+    var style=dglow.style.display;
+        if(style=='block')
+        	{
+            dglow.style.display='none';
+        	}
+    	else{
+            dglow.style.display='block';
+        	}    			
+    }
